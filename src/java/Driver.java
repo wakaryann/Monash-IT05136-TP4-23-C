b194f1a23e230cbcac6511ccf34317ea2650d217
RyanLanders <ryan.landers94@gmail.com>
2023-07-20 19:35:02 +1000

    Ryan doing more testing to make a connection to the database

--- src/java/Driver.java
+++ src/java/Driver.java
@@ -1,14 +1,16 @@
+package src.java;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
 
-public class Main {
+public class Driver {
 
     public static void main(String[] args) {
         // JDBC link provided
-        String connectionString = "jdbc:sqlserver://teamc-jss.database.windows.net:1433;database=manage-jss-cloud;user=jss-admin-testing@teamc-jss;password=TeamC-TP4-2023;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        String connectionString = "jdbc:sqlserver://teamc-jss.database.windows.net:1433;database=manage-jss-cloud;user=jss-admin-testing@teamc-jss;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 
         // Connect to the database
         try (Connection connection = DriverManager.getConnection(connectionString)) {
@@ -16,8 +18,9 @@ public class Main {
 
             // Now that you are connected, you can use the connection for database operations.
 
-            // For example, let's create a simple query to retrieve some data from 'dbo.tbl_dbt_User' table.
-            String sqlQuery = "SELECT * FROM dbo.tbl_dbt_User;";
+            // For example, let's create a simple query to retrieve some data from a table.
+            // Replace 'employees' with the actual table name in your database.
+            String sqlQuery = "SELECT * FROM dbo.tbl_DBT_User; GO";
 
             // Execute the query
             try (Statement statement = connection.createStatement();
@@ -25,11 +28,11 @@ public class Main {
 
                 // Process the query results
                 while (resultSet.next()) {
-                    // Assuming 'dbo.tbl_dbt_User' table has 'username' and 'email' columns
-                    String firstname = resultSet.getString("firstname");
-                    String lastname = resultSet.getString("lastname");
+                    // Assuming 'employees' table has 'name' and 'age' columns
+                    String name = resultSet.getString("name");
+                    int age = resultSet.getInt("age");
 
-                    System.out.println("FirstName: " + firstname + ", LastName: " + lastname);
+                    System.out.println("Name: " + name + ", Age: " + age);
                 }
 
             } catch (SQLException e) {
@@ -41,3 +44,4 @@ public class Main {
         }
     }
 }
+
